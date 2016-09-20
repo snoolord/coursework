efa4de8b07e98c8f5b142f007bc116bef9a0cb06
App Academy Student <student@appacademy.io>
2016-09-20 15:57:58 -0700

    fix error with pieces -> master merge

--- chess/sliding_piece.rb
+++ chess/sliding_piece.rb
@@ -1,26 +1,47 @@
 require_relative 'piece'
 
 module Slidable
-  def horizontal_and_vertical_moves
-    start_pos = self.pos
-    x,y = start_pos
-    possible_moves = []
-    (0..7).each do |index|
-      possible_moves << [index, y]
-      possible_moves << [x, index]
-      p possible_moves
-    end
-    p possible_moves
+  def vertical_and_horizontal_moves
+    x,y = self.pos
+    up, down, left, right = [], [], [], []
+    total_possible_moves = []
+    (x-1).downto(0) { |index| up << [index, y] }
+    ((x + 1)..7).each { |index| down << [index, y] }
+    (y-1).downto(0) { |index| left << [x, index] }
+    (( y + 1 )..7).each { |index| right << [x, index] }
+    total_possible_moves << up << down << left << right
+    total_possible_moves
   end
+
   def diagonal_moves
-    start_pos = self.pos
-    x, y = start_pos
+    x,y = self.pos
     possible_moves = []
-    (0..7).each do |index|
-      possible_moves << [x + index, y + index]
-      possible_moves << [x - index, y + index]
-      possible_moves << [x + index, y - index]
-      possible_moves << [x - index, y - index]
-    end
+    left, right, down_left, down_right = [], [], [], []
+    (1..7).each { |index| left << [x - index, y - index] }
+    (1..7).each { |index| right << [x - index, y + index] }
+    (1..7).each { |index| down_left << [x + index, y - index] }
+    (1..7).each { |index| down_right << [x + index, y + index] }
+    possible_moves << left << right << down_left << down_right
+    possible_moves
   end
+
 end
+#
+# ########
+# require 'slidable'
+#
+# class Rook < Piece
+#   include slidable
+#   def initialize
+#     super("rook")
+#     @move_dirs = [[-1, 0], [1, 0]]
+#   end
+# end
+#
+# # Piece <- SlidingPiece <- Rook
+# #
+# # s = SlidingPiece.new()
+# # s.move
+#
+# r = Rook.new
+# r.move
